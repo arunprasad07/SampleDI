918a5f111358ac7105ce831f898f5bbfe2e59523
arunprasad-07 <arunprasad.azhagesan@gmail.com>
2021-05-24 19:36:19 +0530

    Sample without DI

--- Program.cs
+++ Program.cs
@@ -4,12 +4,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// File is NOT using Depency Injection.
+/// </summary>
+
 namespace SampleDI
 {
+    interface ILog
+    {
+        void write(string message);
+    }
+    class ConsoleLog : ILog
+    {
+        public void write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+    class Car
+    {
+        private Engine engine;
+        private ILog log;
+
+        public Car(Engine engine, ILog log)
+        {
+            this.log = log;
+            this.engine = engine;
+        }
+
+        public void GoForward()
+        {
+            engine.GoAhead(100);
+            log.write("Car going ahead...");
+        }
+    }
+    class Engine
+    {
+        private ILog log;
+        private int id;
+             
+        public Engine(ILog log)
+        {
+            this.log = log;
+            this.id = new Random().Next();
+        }
+
+        public void GoAhead(int power)
+        {
+            log.write($"Engine [{id}] ahead {power}");
+        }
+    }
+   
     class Program
     {
         static void Main(string[] args)
         {
+            var log = new ConsoleLog();
+            var engine = new Engine(log);
+            var car = new Car(engine, log);
+            car.GoForward();
+
+            Console.ReadKey();
         }
     }
 }
